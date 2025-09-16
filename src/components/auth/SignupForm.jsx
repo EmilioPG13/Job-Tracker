70373897a0b05bd328ab148187d4456ad81b83a3
Emilio <emilio.pg95@gmail.com>
2025-09-16 14:27:53 -0600

    🔧 Correct import casing for SocialButton component in SignupForm.jsx

--- src/components/auth/SignupForm.jsx
+++ src/components/auth/SignupForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import SocialButton from './SocialButton';
+import Socialbutton from './Socialbutton';
 import hideIcon from '../../assets/icons/hide.svg';
 import { useNavigate } from 'react-router-dom';
 
